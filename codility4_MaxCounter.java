7fc5d425fa2236d20415af0868e2f7060c909eee
qwertytoki <qwertytoki@gmail.com>
2019-08-12 23:34:11 +0800

    add test method

--- codility4_MaxCounter.java
+++ codility4_MaxCounter.java
@@ -10,10 +10,12 @@ class Solution {
     int maxValue = 0;
     public int[] solution(int N, int[] A) {
         // write your code in Java SE 8
+        N = 100000;
+        A = getTestVal(N);
         int[] counters = new int[N];
         for(int i=0;i<A.length;i++){
             if(A[i]==N+1){
-                counters = setMaxValToAll();
+                counters = setMaxValToAll(N);
             }else{
                 counters[A[i]-1]++;
                 compareMaxValue(counters[A[i]-1]);
@@ -27,9 +29,17 @@ class Solution {
             maxValue = val;    
         }
     }
-    private int[]setMaxValToAll(){
+    private int[]setMaxValToAll(int N){
         int[] counters = new int[N];
+        // 遅さの原因はここ
         Arrays.fill(counters, maxValue);
+        return counters;
+    }
+    private int[] getTestVal(int N){
+        int[] counters = new int[N];
+        for(int i=0;i<counters.length;i++){
+            counters[i] = N+1;
+        }
         return counters;
     }
 }
