a78ace7a2128d2ea98ca9073a10d73c0106a9cd0
qwertytoki <qwertytoki@gmail.com>
2019-08-12 23:55:26 +0800

    performance improved

--- codility4_MaxCounter.java
+++ codility4_MaxCounter.java
@@ -8,37 +8,52 @@ import java.util.Arrays;
 
 class Solution {
     int maxValue = 0;
+
     public int[] solution(int N, int[] A) {
         // write your code in Java SE 8
-        N = 100000;
-        A = getTestVal(N);
+        int maxSetCount = getMaxSetCount(N, A);
+        int currentMaxSet = 0;
         int[] counters = new int[N];
-        for(int i=0;i<A.length;i++){
-            if(A[i]==N+1){
-                counters = setMaxValToAll(N);
-            }else{
-                counters[A[i]-1]++;
-                compareMaxValue(counters[A[i]-1]);
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == N + 1) {
+                currentMaxSet++;
+                if (maxSetCount == currentMaxSet) {
+                    counters = setMaxValToAll(N);
+                }
+            } else {
+                counters[A[i] - 1]++;
+                compareMaxValue(counters[A[i] - 1]);
             }
         }
         return counters;
     }
-    
-    private void compareMaxValue(int val){
-        if(val>maxValue){
-            maxValue = val;    
+
+    private int getMaxSetCount(int N, int[] A) {
+        int maxSetCount = 0;
+        for (int i : A) {
+            if (i == N + 1) {
+                maxSetCount++;
+            }
+        }
+        return maxSetCount;
+    }
+
+    private void compareMaxValue(int val) {
+        if (val > maxValue) {
+            maxValue = val;
         }
     }
-    private int[]setMaxValToAll(int N){
+
+    private int[] setMaxValToAll(int N) {
         int[] counters = new int[N];
-        // 遅さの原因はここ
         Arrays.fill(counters, maxValue);
         return counters;
     }
-    private int[] getTestVal(int N){
+
+    private int[] getTestVal(int N) {
         int[] counters = new int[N];
-        for(int i=0;i<counters.length;i++){
-            counters[i] = N+1;
+        for (int i = 0; i < counters.length; i++) {
+            counters[i] = N + 1;
         }
         return counters;
     }
