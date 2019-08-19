cfa6f4acc0ec6db0c3c77e5dc1bcca7dc5bb89a2
hatano_sh <hatano_sh@worksap.co.jp>
2019-08-19 14:43:06 +0800

    add

--- codility6_MaxProductOfThree.java
+++ codility6_MaxProductOfThree.java
@@ -4,7 +4,7 @@ import java.util.*;
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
-class Solution {
+class Solution { 
     public int solution(int[] A) {
         // write your code in Java SE 8
         Arrays.sort(A);
