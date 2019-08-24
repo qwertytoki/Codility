6852d4155ad8ee77e61bbad382e256ba6edd5c65
qwertytoki <qwertytoki@gmail.com>
2019-08-25 01:53:22 +0800

    first commit

--- codility7_Nesting.java
+++ codility7_Nesting.java
@@ -1,5 +1,6 @@
+
 // you can also use imports, for example:
-// import java.util.*;
+import java.util.*;
 
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
@@ -7,6 +8,17 @@
 class Solution {
     public int solution(String S) {
         // write your code in Java SE 8
-        return 0;
+        Deque<Character> deque = new ArrayDeque<>();
+        for(int i=0;i<S.length();i++){
+            Character c =  S.charAt(i);
+            if(c=='('){
+                deque.push(c);
+            }else if(deque.size()==0){
+                return 0;
+            }else{
+                deque.poll();
+            } 
+        }
+        return deque.size()==0?1:0;
     }
 }
