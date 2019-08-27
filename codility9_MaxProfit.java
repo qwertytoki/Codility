d41a373ca1349648d2a48fdb7063ad0f2d0c006a
qwertytoki <qwertytoki@gmail.com>
2019-08-27 22:46:02 +0800

    second improve

--- codility9_MaxProfit.java
+++ codility9_MaxProfit.java
@@ -13,15 +13,21 @@ class Solution {
          * if min -> max : return max-min
          * if max -> min : compare  max - 2ndMin or 2ndMax - min 
          */
+        if(A.length==0){
+            return 0;
+        }
 
         int[] min = getMin(A);
         int[] max = getMax(A);
         if(min[1]<max[1]){
             return max[0]-min[0];
         }
-        int[] secondMin = getMin(Arrays.copyOfRange(A, 0, max[1]+1));
+        int[] secondMin = getMin(Arrays.copyOfRange(A, 0, max[1]));
         int[] secondMax = getMax(Arrays.copyOfRange(A,min[1],A.length));
-        return Math.max(max[0]-secondMin[0],secondMax[0]-min[0]);
+        int maxProfit =  Math.max(max[0]-secondMin[0],secondMax[0]-min[0]);
+        int[] thirdMin = getMin(Arrays.copyOfRange(A,max[1],min[1]));
+        int[] thirdMax = getMax(Arrays.copyOfRange(A,thirdMin[1],min[1]));
+        return Math.max(maxProfit,thirdMax[0]-thirdMin[0]);
     }
     private int[] getMin(int[] A){
         int[] min = new int[]{Integer.MAX_VALUE,-1};
@@ -31,7 +37,6 @@ class Solution {
                 min[1] = i;
             }
         }
-
         return min;
     }
     private int[] getMax(int[] A){
