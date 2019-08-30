66a083ed6c167dd565c3124f15af66e075ee5b63
qwertytoki <qwertytoki@gmail.com>
2019-08-31 00:12:29 +0800

    java can not pass....

--- codility10_CountFactors2.java
+++ codility10_CountFactors2.java
@@ -1,26 +1,13 @@
-// you can also use imports, for example:
-// import java.util.*;
-
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
 
 class Solution {
     public int solution(int N) {
-        // write your code in Java SE 8
-        // prime factorization
-        // need to improve performance
         int count = 0;
-        for (int i = 1; i <= N; i++) {
-            if (N % i == 0) {
-                if (i * i == N) {
-                    count++;
-                    break;
-                } else if (i * i >= N) {
-                    break;
-                }
-                count += 2;
-            }
+        int i =0;
+        while(i*i<N){
+            if(N%i==0)count+=2;
+            i++;
         }
+        if(i*i==N)count++; 
         return count;
     }
 }
