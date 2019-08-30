e8a8eb21bb72fb1e620d0480fe30e2364f5b61ec
qwertytoki <qwertytoki@gmail.com>
2019-08-30 23:23:04 +0800

    first commit

---
+++ codility10.java
@@ -0,0 +1,18 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int N) {
+        // write your code in Java SE 8
+        // prime factorization
+        // need to improve performance
+        int count = 0;
+        for(int i=1;i<=N;i++){
+            if(N%i==0)count++;
+        }
+        return count;
+    }
+}
