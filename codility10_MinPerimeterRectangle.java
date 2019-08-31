7ddbfb2f1b2065ababbbc67546b2cb2742731f21
qwertytoki <qwertytoki@gmail.com>
2019-08-31 11:16:21 +0800

    improved and got 100

--- codility10_MinPerimeterRectangle.java
+++ codility10_MinPerimeterRectangle.java
@@ -1,5 +1,5 @@
 // you can also use imports, for example:
-// import java.util.*;
+import java.util.*;
 
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
@@ -12,19 +12,12 @@ class Solution {
          * means find minimal N%x=0 and N%y=0, and x*y=N
          * means (i*i<N), x = the closest of i
          */
-        int x =0;
-        int y =0;
-        int squared = 0;
-        for(int i = 1;i<=N;i++){
-            if(i*i>=N){
-                squared = i;
-            }
-        }
-        for(int i= squared;i>1;i--){
+        
+        for(int i= (int)Math.sqrt(N);i>0;i--){
             if(N%i==0){
-                return i*(N/i)*2;
+                return (i+N/i)*2;
             }
         }
-        throw IllegalArgumentException("no matched N");
+        throw new IllegalArgumentException("no matched N");
     }
 }
