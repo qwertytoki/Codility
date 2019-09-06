c0f21f0aa401726f309f1afcfd7fb9c8854db9f1
qwertytoki <qwertytoki@gmail.com>
2019-09-06 23:38:16 +0800

    15

---
+++ codility15_CountTriangles.java
@@ -0,0 +1,11 @@
+
+// you can also use imports, for example:
+import java.util.*;
+
+// need to improve performance
+class Solution {
+    public int solution(int M, int[] A) {
+        
+        return 0;
+    }
+}
