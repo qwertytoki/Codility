39972e98fb79202e46778e048bce1aaf013aad0a
qwertytoki <qwertytoki@gmail.com>
2019-09-07 17:28:54 +0800

    second try fail...

--- codility15_CountDistinctSlices2.java
+++ codility15_CountDistinctSlices2.java
@@ -1,24 +1,31 @@
 
-// you can also use imports, for example:
 import java.util.*;
-
-// need to improve performance
 class Solution {
     public int solution(int M, int[] A) {
+        // write your code in Java SE 8
         /**
-         * 
+         * this is the permutation problem
+         * add xPx patterns while the number is not duplicated.
+         * in other words, add own index patterns if it's not duplicated. 
          */
+        //  map contains his number and index
+        final int LIMIT = 1000000000;
+        Map<Integer,Integer>map =  new HashMap<>();
+        
         int count = 0;
-        int lastIndex = -1;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < A.length; i++) {
-            int preOccurIndex = map.containsKey(A[i]) ? map.get(A[i]) : -1;
-            lastIndex = Math.max(preOccurIndex, lastIndex);
-            count += i - lastIndex;
-            if (count >= 1000000000) {
-                return 1000000000;
+        int index = 0;
+        for(int i:A){
+            if(map.get(i)==null){
+                index++;
+            }else{
+                index = 1;
+                map = new HashMap<>();
             }
-            map.put(A[i], i);
+                count += index;
+                map.put(i,index);
+                if(count>=LIMIT){
+                    return LIMIT;
+                }
         }
         return count;
     }
