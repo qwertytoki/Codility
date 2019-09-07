a13601c051264658a34d9550f11e9efbee3d1941
qwertytoki <qwertytoki@gmail.com>
2019-09-07 22:24:26 +0800

    blute force need to improve speed

--- codility15_CountTriangles.java
+++ codility15_CountTriangles.java
@@ -1,11 +1,25 @@
-
 // you can also use imports, for example:
 import java.util.*;
 
-// need to improve performance
 class Solution {
-    public int solution(int M, int[] A) {
+    public int solution(int[] A) {
         
-        return 0;
+        /**
+         * find trianguable triplet 
+         * largest < smallest + middle : trianguable
+         */
+        if(A.length<3)return 0;
+        //blute force
+        int count =0;
+        Arrays.sort(A);
+        for(int i =0;i<A.length;i++){
+            if(i+3>A.length)break;
+            for(int j=i+1;j<A.length;j++){
+                for(int k=j+1;k<A.length;k++){
+                    if(A[i]+A[j]>A[k])count++;
+                }
+            }
+        }
+        return count;
     }
 }
