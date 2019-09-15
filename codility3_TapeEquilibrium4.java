8c84088b79dab6572a053e0a4dc40284e71dcfd7
qwertytoki <qwertytoki@gmail.com>
2019-09-15 22:16:13 +0800

    4th challenge

--- codility3_TapeEquilibrium4.java
+++ codility3_TapeEquilibrium4.java
@@ -1,28 +1,25 @@
 // you can also use imports, for example:
 import java.util.*;
-
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
-
 class Solution {
     public int solution(int[] A) {
-        // write your code in Java SE 8
         /**
          * look for smallest divide place
-         * create 2 valiables. one start from 0, the other start from end.
          * compare and record minimum value
          */
         int sum = 0;
-        int minVal = Integer.MAX_VALUE;
         for(int i:A){
             sum+=i;
         }
-        for(int i:A){
-            sum-=i*2;
+        int minVal = sum - A[0]*2;
+        if(minVal<0)
+            minVal= minVal*-1;
+        
+        for(int i=1;i<A.length-1;i++){
+            sum-=A[i]*2;
             sum = sum<0 ? sum*-1:sum;
             minVal = Math.min(minVal,sum);
+            
         }
-
         return minVal;
     }
 }
